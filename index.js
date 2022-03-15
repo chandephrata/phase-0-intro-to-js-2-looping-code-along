5bb7902d48f68065c58db962129284f3bd609e43
Chandelier Ephrata <chandephrata@gmail.com>
2022-03-15 16:59:32 -0600

    commit tree e39504d2adca36e9165fdec5a4c41e769eef0358

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const gifts = ["teddy bear", "drone", "doll"];
+const gifts = ["teddy bear", "drone", "doll"]; // the initialization moves OUTSIDE the body of the loop!
 
-function wrapGifts(gifts) {
-  let i = 0; // the initialization moves OUTSIDE the body of the loop!
-  while (i < gifts.length) {
+function wrapGifts(gifts) {  
+  for (let i = 0; i < gifts.length; i++) {       // the iteration moves INSIDE the body of the loop!
     console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    i++; // the iteration moves INSIDE the body of the loop!
   }
 
   return gifts;
@@ -15,3 +13,27 @@ wrapGifts(gifts);
 // LOG: Wrapped drone and added a bow!
 // LOG: Wrapped doll and added a bow!
 // => ["teddy bear", "drone", "doll"]
+
+const names=[]
+
+function writeCards(names,birthday){
+  let newArray =[...names];
+  for (let i = 0; i < names.length; i++){
+   newArray.push(`Thank you , ${names[i]}, for the wonderful ${birthday} gifts!`);
+  }
+  return newArray;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "suprise");
+
+ function countDown(num){
+     let number = num;
+     while (number >= 0){
+         console.log(number);
+         number --;
+     }
+ }
+
+
+
+ 
